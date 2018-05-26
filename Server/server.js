eead488937faeb0b1f6371033866f1d23a7158f6
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2018-05-25 22:27:54 -0500

    Add files via upload

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,6 @@
+const express = require('express')
+const app = express()
 
+app.get('/', (req, res) => res.send('COSSbot Localhost Server'))
+
+app.listen(4000, () => console.log('COSSbot Server running on port 4000!'))
