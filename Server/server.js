1e67891cd0a092349a1689aae88a26966f90296e
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2018-05-28 02:16:43 -0500

    commited latest changes to server.js

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,25 @@
 const express = require('express')
+var myParser = require('body-parser')
 const app = express()
 
 app.get('/', (req, res) => res.send('COSSbot Localhost Server'))
+app.use(myParser.urlencoded({extended : true}));
+
+app.use(myParser.json());
+
+app.post("/data1", function(request, response) {
+    console.log(('data1- ') + (request.body.name) + (': ') + (request.body.value));
+    //TODO set var data1 in parent process = (request.body.value) 
+    });
+
+app.post("/data2", function(request, response) {
+    console.log(('data2- ') + (request.body.name) + (': ') + (request.body.value));
+    //TODO set var data2 in parent process = (request.body.value) 
+    });
+
+app.post("/data3", function(request, response) {
+    console.log(('data3- ') + (request.body.name) + (': ') + (request.body.value));
+    });
 
 app.listen(4000, () => console.log('COSSbot Server running on port 4000!'))
+
