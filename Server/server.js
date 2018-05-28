78781a1ecabe41943d488e04133f313471283a30
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2018-05-28 06:17:41 -0500

    updated server.js

--- Server/server.js
+++ Server/server.js
@@ -2,27 +2,35 @@ const express = require('express')
 var myParser = require('body-parser')
 const app = express()
 
+var data1;
+var data2;
+var data3;
+
 app.get('/', (req, res) => res.send('COSSbot Localhost Server'))
 app.use(myParser.urlencoded({extended : true}));
 
 app.use(myParser.json());
 
 app.post("/data1", function(request, response) {
-    console.log((request.body.name) + (': ') + (request.body.value));
-    //TODO set var data1 in parent process = (request.body.value) 
-    //AFTER TODO IS COMPLETED, REMOVE CONSOLE.LOG
+    data1 = (request.body.value);
+    //TODO set var data1 in parent process (main.js) = (request.body.value) 
+
+    console.log(data1);
     });
 
 app.post("/data2", function(request, response) {
-    console.log((request.body.name) + (': ') + (request.body.value));
-    //TODO set var data2 in parent process = (request.body.value) 
-    //AFTER TODO IS COMPLETED, REMOVE CONSOLE.LOG
+    data2 = (request.body.value);
+    //TODO set var data2 in parent process (main.js) = (request.body.value)
+   
+    console.log(data2);
+    
     });
 
 app.post("/data3", function(request, response) {
-    console.log((request.body.name) + (': ') + (request.body.value));
-    //TODO set var data3 in parent process = (request.body.value) 
-    //AFTER TODO IS COMPLETED, REMOVE CONSOLE.LOG
+    data3 = (request.body.value);
+    //TODO set var data3 in parent process (main.js) = (request.body.value) 
+    
+    console.log(data3);
     });
 
 app.listen(4000, () => console.log('COSSbot Server running on port 4000!'))
